be66a961792ead55f8d9456e29b765f17148022d
hcouto20 <helder@brassun.com.br>
2021-02-09 11:37:07 -0300

    Implementando module pokemon em trainer

---
+++ lib/ex_mon/trainer/pokemon.ex
@@ -0,0 +1,18 @@
+defmodule ExMon.Trainer.Pokemon do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias ExMon.Trainer
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  @foreign_key_type Ecto.UUID
+
+  schema "pokemons" do
+    field(:name, :string)
+    field(:nickname, :string)
+    field(:weight, :integer)
+    field(:types, {:array, :string})
+    belongs_to(:trainer, Trainer)
+    timestamps()
+  end
+end
